3af1b640fb55f457867e128188e7e8141d700cb6
Masodi Success <109852676+suskidee@users.noreply.github.com>
2024-11-11 21:26:23 +0100

    changed views.py

--- api/views.py
+++ api/views.py
@@ -16,45 +16,6 @@ from rest_framework.response import Response
 from rest_framework import viewsets, status
 from .seralizers import *
 
-
-def initiate_payment(amount, email, pending_id, user):
-    url = "https://api.flutterwave.com/v3/payments"
-    headers = {
-        "Authorization": f"Bearer {settings.FLW_SEC_KEY}"
-    }
-    first_name = user.first_name
-    last_name = user.last_name
-    data = {
-        "tx_ref": str(uuid.uuid4()),
-        "amount": str(amount),
-        "currency": "NGN",
-        "redirect_url": "http:/127.0.0.1:8000/api/pending/confirm_payment/?p_id=" + pending_id,
-        "meta": {
-            "consumer_id": 23,
-            "consumer_mac": "92a3-912ba-1192a"
-        },
-        "customer": {
-            "email": email,
-            "phonenumber": "080****4528",
-            "name": f"{last_name} {first_name}"
-        },
-        "customizations": {
-            "title": "Success Air",
-            "logo": "https://jetinternational.com/wp-content/uploads/2016/11/iStock-177708665-5.jpg"
-
-        }
-    }
-
-    try:
-        response = requests.post(url, headers=headers, json=data)
-        response_data = response.json()
-        return Response(response_data)
-
-    except requests.exceptions.RequestException as err:
-        print("the payment didn't go through")
-        return Response({"error": str(err)}, status=500)
-
-
 class ApiRoute(viewsets.ModelViewSet):
     serializer_class = RouteSerializer
     permission_classes = [IsAdminOrReadOnly]

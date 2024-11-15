5d1a950e32f8fa17cf748cf4b87ccb60aff25d52
Masodi Success <109852676+suskidee@users.noreply.github.com>
2024-11-15 15:54:37 +0100

    working on urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,23 @@
-# from django.urls import path, include
-# from . import views
-# from rest_framework.routers import DefaultRouter
-# from rest_framework_nested import routers
-#
-# router = DefaultRouter()
-# router.register('route', views.ApiRoute, basename='route')
-# router.register('cities', views.ApiCities, basename='cities')
-# router.register('pending', views.ApiPending, basename='pending')
-# router.register('booking', views.ApiBooking, basename='booking')
-#
-#
-# urlpatterns = [
-#     path("", include(router.urls)),
-# ]
-#
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('route', views.ApiSold, basename='route')
+router.register('cities', views.ApiSalaryWorkers, basename='cities')
+router.register('pending', views.ApiContractors, basename='pending')
+router.register('booking', views.ContractorsSerializer, basename='booking')
+router.register('booking', views.ApiQuotation, basename='booking')
+router.register('booking', views.ApiRemoved, basename='booking')
+router.register('booking', views.ApiProduct, basename='booking')
+router.register('booking', views.ApiProject, basename='booking')
+router.register('booking', views.ApiRemoved, basename='booking')
+router.register('booking', views.ApiRemoved, basename='booking')
+router.register('booking', views.ApiRemoved, basename='booking')
+router.register('booking', views.ApiRemoved, basename='booking')
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+

499bbcaf0daa26d2ec49814302e8784bf1e92603
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-01-20 15:57:36 +0100

    made first and last name into name

--- customers/models.py
+++ customers/models.py
@@ -2,18 +2,17 @@ from django.db import models
 
 
 class Customer(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
+    name = models.CharField(unique=True,max_length=100)
+    email = models.EmailField(unique=True, null=True, blank=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.name}"
 
     class Meta:
-        ordering = ["last_name", "first_name"]
+        ordering = ["name"]
 
 
 # class Payment(models.Model):

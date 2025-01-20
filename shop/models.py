ff7aa3291f764e12d82364199b3cf4a95a7212ec
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-01-20 15:13:39 +0100

    added customer and updated on field

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from customers.models import Customer
 
 
 class InventoryItem(models.Model):
@@ -23,9 +24,11 @@ class InventoryItem(models.Model):
 
 
 class Sold(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
     item = models.ForeignKey(InventoryItem, on_delete=models.PROTECT)
     quantity = models.PositiveIntegerField()
     date = models.DateField(auto_now_add=True)
+    updated_on = models.DateField(null=True, blank=True)
 
     @property
     def total_price(self):

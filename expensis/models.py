7582735696345531171529bb5c6ede3f030d8446
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-01-21 14:48:09 +0100

    added quantity to expensis

--- expensis/models.py
+++ expensis/models.py
@@ -5,6 +5,7 @@ class Expense(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
     date = models.DateField()
 
     def __str__(self):

ff189f1b547347ee97236304b33f6e7eda6f9ee1
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-01-22 21:39:34 +0100

    made auto add now in models true

--- expensis/models.py
+++ expensis/models.py
@@ -6,7 +6,7 @@ class Expense(models.Model):
     description = models.TextField(null=True, blank=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()
-    date = models.DateField()
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.description})"

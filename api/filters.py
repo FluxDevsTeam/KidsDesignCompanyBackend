a6c9d4f14f99a9c7e8730333fa487c7139ef8afb
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-01-24 12:53:53 +0100

    made filter work with default (current month)

--- api/filters.py
+++ api/filters.py
@@ -4,25 +4,25 @@ from datetime import datetime
 
 
 class ExpenseFilter(django_filters.FilterSet):
-    month = django_filters.NumberFilter(field_name='date__month', method='filter_by_month')
-    year = django_filters.NumberFilter(field_name='date__year', method='filter_by_year')
-    day = django_filters.NumberFilter(field_name='date__day', method='filter_by_day')
+    month = django_filters.NumberFilter(field_name='date__month', method='filter_by_month', required=False)
+    year = django_filters.NumberFilter(field_name='date__year', method='filter_by_year', required=False)
+    day = django_filters.NumberFilter(field_name='date__day', method='filter_by_day', required=False)
 
     class Meta:
         model = Expense
         fields = ['month', 'year', 'day']
 
     def filter_by_month(self, queryset, name, value):
-        if value:
-            return queryset.filter(date__month=value)
-        return queryset
+        if not value:
+            value = datetime.now().month
+        return queryset.filter(date__month=value)
 
     def filter_by_year(self, queryset, name, value):
-        if value:
-            return queryset.filter(date__year=value)
-        return queryset
+        if not value:
+            value = datetime.now().year
+        return queryset.filter(date__year=value)
 
     def filter_by_day(self, queryset, name, value):
-        if value:
-            return queryset.filter(date__day=value)
-        return queryset
+        if not value:
+            value = datetime.now().day
+        return queryset.filter(date__day=value)

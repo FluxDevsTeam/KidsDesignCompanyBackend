a9dacb00619c22dbefe5568fa3a442357b773146
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-01-24 11:10:08 +0100

    made date datetimefield

--- expensis/models.py
+++ expensis/models.py
@@ -6,7 +6,7 @@ class Expense(models.Model):
     description = models.TextField(null=True, blank=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.CharField(max_length=200)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.description})"

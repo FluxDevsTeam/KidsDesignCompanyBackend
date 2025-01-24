601e932c5d71d47aec8902ad683b8f9b7882a0b8
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-01-24 10:57:51 +0100

    made quantity charfield so unit can be added

--- expensis/models.py
+++ expensis/models.py
@@ -5,7 +5,7 @@ class Expense(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.IntegerField()
+    quantity = models.CharField(max_length=200)
     date = models.DateField(auto_now_add=True)
 
     def __str__(self):

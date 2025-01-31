d5d848d2e1097ee92bf945fc6ff434f4f21936ba
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-01-31 14:01:48 +0100

    changed ProductContractor to connect to worksers app with many to many fields

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from workers.models import Contractors, SalaryWorkers
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     quantity = models.PositiveIntegerField()
@@ -8,20 +9,20 @@ class Product(models.Model):
     dimensions = models.CharField(max_length=50)
     colour = models.CharField(max_length=50)
     design = models.TextField()
-    contractor = models.ForeignKey(Contractors, on_delete=models.PROTECT)
-    contractor_cost = models.DecimalField(max_digits=10, decimal_places=2)
-    salary_worker = models.ForeignKey(SalaryWorkers, on_delete=models.PROTECT)
-    salary_worker_cost = models.DecimalField(max_digits=10, decimal_places=2)
-
+    contractors = models.ManyToManyField(Contractors, through='ProductContractor')
+    salary_workers = models.ManyToManyField(SalaryWorkers, through='ProductSalaryWorker')
     selling_price = models.DecimalField(max_digits=10, decimal_places=2)
     cost_price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def total_production_cost(self):
-        return self.cost_price + self.contractor_cost
+        total_cost = self.cost_price
+        total_cost += sum([pc.cost for pc in self.productcontractor_set.all()])
+        total_cost += sum([psw.cost for psw in self.productsalaryworker_set.all()])
+        return total_cost
 
     @property
     def profit(self):
-        return (self.selling_price - self.cost_price) * self.quantity
+        return (self.selling_price - self.total_production_cost()) * self.quantity
 
     def __str__(self):
         return self.name
@@ -57,3 +58,21 @@ class Quotation(models.Model):
 
     def __str__(self):
         return f"quotation for - {self.product.name}"
+
+
+class ProductContractor(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    contractor = models.ForeignKey(Contractors, on_delete=models.PROTECT)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)  # Individual pay
+
+    def __str__(self):
+        return f"{self.contractor.name} for {self.product.name}"
+
+
+class ProductSalaryWorker(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    salary_worker = models.ForeignKey(SalaryWorkers, on_delete=models.PROTECT)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)  # Individual pay
+
+    def __str__(self):
+        return f"{self.salary_worker.name} for {self.product.name}"

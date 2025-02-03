f83029ebb96945175ae4e478450e3a068a47981d
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-02-03 13:57:49 +0100

    added store category

--- store/models.py
+++ store/models.py
@@ -2,18 +2,25 @@ from django.db import models
 from products.models import Product
 
 
+class StoreCategory(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class RawMaterial(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     unit = models.CharField(max_length=20)
     quantity = models.DecimalField(max_digits=10, decimal_places=2)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def cost_per_unit(self):
-        return self.price / self.quantity
-
-    category = models.CharField(max_length=50)
+    category = models.ForeignKey(StoreCategory, on_delete=models.PROTECT)
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to="raw_materials/", blank=True, null=True)
+
+    @property
+    def cost_per_unit(self):
+        return f"{self.price / self.quantity} /{self.unit}"
 
     def __str__(self):
         return self.name

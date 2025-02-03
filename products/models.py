2cf9216fbcd8ae296db9b0977ee40ea98c7574d3
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-02-03 13:56:40 +0100

    added quantity

--- products/models.py
+++ products/models.py
@@ -1,19 +1,23 @@
 from django.db import models
 from workers.models import Contractors, SalaryWorkers
+from project.models import Project
 
 
 class Product(models.Model):
+    # project = models.ForeignKey(Project, on_delete=models.PROTECT)
+    # quantity = models.IntegerField(default=1)
     name = models.CharField(max_length=100)
     quantity = models.PositiveIntegerField()
     images = models.ImageField(upload_to="product/", blank=True, null=True)
     dimensions = models.CharField(max_length=50)
     colour = models.CharField(max_length=50)
     design = models.TextField()
-    contractors = models.ManyToManyField(Contractors, through='ProductContractor')
-    salary_workers = models.ManyToManyField(SalaryWorkers, through='ProductSalaryWorker')
+    # contractors = models.ManyToManyField(Contractors, through='ProductContractor')
+    # salary_workers = models.ManyToManyField(SalaryWorkers, through='ProductSalaryWorker')
     selling_price = models.DecimalField(max_digits=10, decimal_places=2)
     overhead_cost = models.DecimalField(max_digits=10, decimal_places=2)
     production_note = models.TextField()
+
     @property
     def total_production_cost(self):
         total_cost = 5

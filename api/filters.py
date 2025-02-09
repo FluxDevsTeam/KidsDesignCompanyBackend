00473e00ed117c3480282b6448d42a3cade45b75
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-02-09 22:59:33 +0100

    added models to created new filter for apiviews

--- api/filters.py
+++ api/filters.py
@@ -1,8 +1,12 @@
-import django_filters
 from expensis.models import Expense
 from datetime import datetime
 import django_filters
-from shop.models import InventoryItem, AddStock
+
+from project.models import Project
+from shop.models import InventoryItem, AddStock, Sold
+import datetime
+
+from store.models import AddRawMaterials
 
 
 class AddStockFilter(django_filters.FilterSet):
@@ -12,6 +16,31 @@ class AddStockFilter(django_filters.FilterSet):
 
     class Meta:
         model = AddStock
+        fields = ['month', 'year', 'day']
+
+    def filter_by_month(self, queryset, name, value):
+        if not value:
+            value = datetime.now().month
+        return queryset.filter(date__month=int(value))
+
+    def filter_by_year(self, queryset, name, value):
+        if not value:
+            value = datetime.now().year
+        return queryset.filter(date__year=int(value))
+
+    def filter_by_day(self, queryset, name, value):
+        if not value:
+            value = datetime.now().day
+        return queryset.filter(date__day=int(value))
+
+
+class AddRawMaterialsFilter(django_filters.FilterSet):
+    month = django_filters.NumberFilter(field_name='date__month', method='filter_by_month', required=False)
+    year = django_filters.NumberFilter(field_name='date__year', method='filter_by_year', required=False)
+    day = django_filters.NumberFilter(field_name='date__day', method='filter_by_day', required=False)
+
+    class Meta:
+        model = AddRawMaterials
         fields = ['month', 'year', 'day']
 
     def filter_by_month(self, queryset, name, value):
@@ -55,6 +84,31 @@ class ExpenseFilter(django_filters.FilterSet):
         return queryset.filter(date__day=int(value))
 
 
+class SoldFilter(django_filters.FilterSet):
+    month = django_filters.NumberFilter(field_name='date__month', method='filter_by_month', required=False)
+    year = django_filters.NumberFilter(field_name='date__year', method='filter_by_year', required=False)
+    day = django_filters.NumberFilter(field_name='date__day', method='filter_by_day', required=False)
+
+    class Meta:
+        model = Sold
+        fields = ['month', 'year', 'day']
+
+    def filter_by_month(self, queryset, name, value):
+        if not value:
+            value = datetime.now().month
+        return queryset.filter(date__month=int(value))
+
+    def filter_by_year(self, queryset, name, value):
+        if not value:
+            value = datetime.now().year
+        return queryset.filter(date__year=int(value))
+
+    def filter_by_day(self, queryset, name, value):
+        if not value:
+            value = datetime.now().day
+        return queryset.filter(date__day=int(value))
+
+
 class InventoryItemFilter(django_filters.FilterSet):
     archived = django_filters.BooleanFilter(field_name='archived')
     empty_stock = django_filters.BooleanFilter(method='filter_empty_stock', label='Empty Stock')
@@ -74,4 +128,28 @@ class InventoryItemFilter(django_filters.FilterSet):
     def filter_low_stock(self, queryset, name, value):
         if value:
             return queryset.filter(stock__lt=10, archived=False)
+        return queryset
+
+
+class ProjectFilter(django_filters.FilterSet):
+    archived = django_filters.BooleanFilter(field_name='archived')
+    deadline = django_filters.BooleanFilter(method='filter_deadline', label='Past Deadline')
+    upcoming_deadline = django_filters.BooleanFilter(method='filter_upcoming_deadline', label='Deadline within 2 Weeks')
+    is_delivered = django_filters.BooleanFilter(field_name='is_delivered')
+
+    class Meta:
+        model = Project
+        fields = ['archived', 'is_delivered']
+
+    def filter_deadline(self, queryset, name, value):
+        if value:
+            today = datetime.date.today()
+            return queryset.filter(deadline__lt=today)
+        return queryset
+
+    def filter_upcoming_deadline(self, queryset, name, value):
+        if value:
+            today = datetime.date.today()
+            two_weeks = today + datetime.timedelta(days=14)
+            return queryset.filter(deadline__lte=two_weeks)
         return queryset

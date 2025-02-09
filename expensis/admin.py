2aeeb56efcce336a9f6702b1d7d6249faa590d41
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-02-09 22:57:38 +0100

    added models to admin.py

--- expensis/admin.py
+++ expensis/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Expense
+from .models import Expense, Assets, ExpenseCategory
 # Register your models here.
 admin.site.register(Expense)
+admin.site.register(Assets)
+admin.site.register(ExpenseCategory)

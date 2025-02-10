1e23fd0d4ee5c090f0285319cd5f7941a886c7ae
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-02-10 08:03:19 +0100

    added OtherProduction

--- project/models.py
+++ project/models.py
@@ -1,5 +1,4 @@
 import datetime
-
 from django.db import models
 from customers.models import Customer
 from decimal import Decimal
@@ -12,21 +11,21 @@ class Project(models.Model):
     invoice_image = models.ImageField(upload_to="project_invoice/", blank=True, null=True)
     status = models.CharField(max_length=50, default="Not started")
     start_date = models.DateField(default=datetime.date.today)
+    all_items = models.JSONField(blank=True, null=True)
     deadline = models.DateField(blank=True, null=True)
-    progress = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)], help_text="Progress as a whole number percentage (0 to 100).")
+    progress = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)],help_text="Progress as a whole number percentage (0 to 100).")
     date_delivered = models.DateField(blank=True, null=True)
-    selling_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    logistics = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    service_charge = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     is_delivered = models.BooleanField(default=False)
     archived = models.BooleanField(default=False)
-
+    selling_price = models.DecimalField(max_digits=15, decimal_places=2, default=0)
+    logistics = models.DecimalField(max_digits=15, decimal_places=2, default=0)
+    service_charge = models.DecimalField(max_digits=15, decimal_places=2, default=0)
+    note = models.TextField(null=True, blank=True)
+    balance = models.DecimalField(max_digits=15, decimal_places=2, default=0)
+    
     @property
     def timeframe(self):
-        return (self.deadline - self.start_date).days
-
-    def total_cost(self):
-        return sum(self.products.total_production_cost) + sum(self.shop_items.selling_price) + self.running_expenses + self.other_expensis
+        return (self.deadline - self.start_date).days if self.deadline else None
 
     def __str__(self):
         return f"Project {self.name}"
@@ -41,3 +40,14 @@ class OverheadCost(models.Model):
 
     def __str__(self):
         return f"Overhead Cost Base: {self.overhead_cost_base}"
+
+
+class OtherProduction(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    budget = models.DecimalField(max_digits=15, decimal_places=2, default=Decimal("0.00"))
+    cost = models.DecimalField(max_digits=15, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+

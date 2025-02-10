6e62827702ae80d6f86bbfd759173888a31b2443
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-02-10 08:04:15 +0100

    created assets table

--- expensis/models.py
+++ expensis/models.py
@@ -1,4 +1,9 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import Sum
+
+from project.models import Project
+from shop.models import Sold
 
 
 class ExpenseCategory(models.Model):
@@ -11,6 +16,8 @@ class ExpenseCategory(models.Model):
 class Expense(models.Model):
     name = models.CharField(max_length=200)
     category = models.ForeignKey(ExpenseCategory, on_delete=models.PROTECT, null=True, blank=True)
+    project = models.ForeignKey(Project, on_delete=models.PROTECT, null=True, blank=True)
+    shop = models.ForeignKey(Sold, on_delete=models.PROTECT, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.CharField(max_length=200)
@@ -19,5 +26,28 @@ class Expense(models.Model):
     def __str__(self):
         return f"{self.description})"
 
+    def clean(self):
+        if self.project and self.shop:
+            raise ValidationError("Expense cannot be associated with both a project and a shop item.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
     class Meta:
         ordering = ["-date"]
+
+
+class Assets(models.Model):
+    name = models.CharField(max_length=50)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+    expected_lifespan = models.CharField(max_length=50)
+    is_still_available = models.BooleanField(default=True)
+
+    @classmethod
+    def get_total_value(cls):
+        result = cls.objects.filter(is_still_available=True).aggregate(total=Sum('value'))
+        return result['total'] or 0
+
+    def __str__(self):
+        return f"{self.name} -- {self.value}"

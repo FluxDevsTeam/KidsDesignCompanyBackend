13fb1b29c1c2f2ee43c0f229391378f189d69a77
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-03-04 23:48:29 +0100

    edited models

--- shop/models.py
+++ shop/models.py
@@ -15,7 +15,7 @@ class InventoryCategory(models.Model):
 
 class InventoryItem(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    category = models.ForeignKey(InventoryCategory, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey(InventoryCategory, on_delete=models.SET_NULL, null=True, blank=True)
     description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to="shop/", blank=True, null=True)
     stock = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, validators=[MinValueValidator(0.01)])

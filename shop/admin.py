82f822cab6f31d8ebd079df0f6568f4287e6b824
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-03-13 09:33:06 +0100

    added admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import InventoryItem, Sold
+from .models import InventoryItem, Sold, AddStock
 # Register your models here.
 
 admin.site.register(InventoryItem)
 admin.site.register(Sold)
+admin.site.register(AddStock)
 

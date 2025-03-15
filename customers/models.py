e2b7a950b074d3252bb6d16271b1e8748578c5dd
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-03-15 20:43:58 +0100

    refactor: change created_at field in Customer model to DateField

--- customers/models.py
+++ customers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 
 class Customer(models.Model):
@@ -6,7 +7,7 @@ class Customer(models.Model):
     email = models.EmailField(unique=True, null=True, blank=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateField(default=date.today)
 
     def __str__(self):
         return f"{self.name}"

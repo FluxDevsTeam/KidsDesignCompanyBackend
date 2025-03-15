5076a703274c6ec2df97639354ec6817c215586e
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-03-15 20:43:15 +0100

    refactor: register ProductContractor and additional worker models in admin interface

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product, Quotation
+from .models import Product, Quotation, ProductContractor
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(Quotation)
+admin.site.register(ProductContractor)
 

7c95e5a22a77663e539b0ab061f3324f578b5263
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-03-16 23:57:42 +0100

    edited store admin

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import RawMaterial, Removed
+from .models import RawMaterial, Removed, StoreCategory, AddRawMaterials
+
 # Register your models here.
 admin.site.register(RawMaterial)
+admin.site.register(StoreCategory)
+admin.site.register(AddRawMaterials)
 admin.site.register(Removed)
 

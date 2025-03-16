01812ca391b350b63df07110720591fb09b46fa1
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-03-16 23:55:10 +0100

    edited products admin

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Product, Quotation, ProductContractor
+from .models import Product, Quotation, ProductContractor, ProductSalaryWorker
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(Quotation)
 admin.site.register(ProductContractor)
+admin.site.register(ProductSalaryWorker)
 

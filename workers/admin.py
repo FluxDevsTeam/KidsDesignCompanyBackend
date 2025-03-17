8d7bc9e9b13ef3b53b30114df38d502f02213bf9
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-03-17 01:21:49 +0100

    edited workers admin

--- workers/admin.py
+++ workers/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Contractors, SalaryWorkers, Paid
+from .models import Contractors, SalaryWorkers, Paid, SalaryWorkersRecord, ContractorRecord
 
 # Register your models here.
 admin.site.register(Contractors)
 admin.site.register(SalaryWorkers)
 admin.site.register(Paid)
+admin.site.register(SalaryWorkersRecord)
+admin.site.register(ContractorRecord)

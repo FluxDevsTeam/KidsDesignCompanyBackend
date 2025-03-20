c8d32554b6d15b6d4b7e48d4d0e393a867033743
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-03-20 09:03:35 +0100

    added multiple servers to schema

--- base/urls.py
+++ base/urls.py
@@ -23,12 +23,17 @@ from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
 schema_view = get_schema_view(
     openapi.Info(
         title="KidsDesignCompanyBackend",
         default_version='v1',
-        description="An API for kids design company backend",
+        description="""
+        An API for Kids Design Company backend.
+
+        **Servers:**
+        - Local: [http://localhost:8000](http://localhost:8000)
+        - Production: [https://kidsdesigncompany.pythonanywhere.com/](https://kidsdesigncompany.pythonanywhere.com/)
+        """,
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="suskidee@gmail.com"),
         license=openapi.License(name="Test License"),

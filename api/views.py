8af70902e1efc6981b57793c7f1230fd2b92dafa
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-06-25 19:51:09 +0100

    added admin to new roles

--- api/views.py
+++ api/views.py
@@ -44,7 +44,7 @@ class ApiInventoryItem(ModelViewSet):
     pagination_class = PageNumberPagination
 
     permission_classes = [CheckUserRoles]
-    required_roles = ['shopkeeper','ceo']
+    required_roles = ['shopkeeper', 'ceo', 'admin']
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -469,7 +469,7 @@ class ApiSold(ModelViewSet):
     search_fields = ['item__name', 'customer__name']
 
     permission_classes = [CheckUserRoles]
-    required_roles = ['shopkeeper','ceo']
+    required_roles = ['shopkeeper', 'ceo']
 
     def list(self, request, *args, **kwargs):
         today = timezone.now().date()
@@ -1107,7 +1107,7 @@ class ApiProject(ModelViewSet):
     search_fields = ['customer__name', 'name']
     ordering = ['progress', "deadline"]
     permission_classes = [CheckUserRoles]
-    required_roles = ['factory_manager', 'project_manager', 'ceo', 'shopkeeper']
+    required_roles = ['factory_manager', 'project_manager', 'ceo', 'shopkeeper', 'admin']
 
     def get_queryset(self):
         qs = super().get_queryset()

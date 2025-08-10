95cbd32b4713dcd0aedcc94b6f94d796b0eb6715
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-08-10 16:44:54 +0100

    edited model to be nullable

--- expensis/models.py
+++ expensis/models.py
@@ -31,7 +31,7 @@ class Expense(models.Model):
     description = models.TextField(null=True, blank=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.CharField(max_length=200)
-    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHODS)
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHODS, null=True, blank=True)
     date = models.DateField(default=date.today)
 
     def __str__(self):

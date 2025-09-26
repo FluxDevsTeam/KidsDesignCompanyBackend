97b191363b232cd926dea7e7a9af1b820dfc0b56
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-09-26 12:08:47 +0100

    Refactor app names to include 'apps.' prefix in configuration and settings

--- apps/expensis/apps.py
+++ apps/expensis/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ExpensisConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'expensis'
+    name = 'apps.expensis'
--- apps/workers/apps.py
+++ apps/workers/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AtrisansConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'workers'
+    name = 'apps.workers'
